5d901ca6a2aa3de9fc014c53655934d37d721a10
Chris Chapman <chris@pentandra.com>
2024-08-12 14:00:36 -0600

    Parent tax does not know sourcing

--- scripts/import_rates.py
+++ scripts/import_rates.py
@@ -141,7 +141,6 @@ def update_taxes(code, taxes):
             name = '%s %s' % (code_fips, type_)
             description = '%s tax (%s)' % (code_fips
                     if jurisdiction is None else jurisdiction.name, row[type_])
-            sourcing = 'intrastate' if 'intrastate' in type_ else 'interstate'
             rate_type = 'general' if 'general' in type_ else 'food'
 
             if current_code_fips != code_fips:
@@ -155,7 +154,7 @@ def update_taxes(code, taxes):
                 parent.authority = authority
                 parent.type = 'none'
                 parent.group = groups[row['jurisdiction_type']]
-                parent.sourcing = sourcing
+                parent.sourcing = None
                 parent.rate_type = rate_type
                 records.append(parent)
 
@@ -171,7 +170,7 @@ def update_taxes(code, taxes):
             record.type = 'percentage'
             record.group = groups[row['jurisdiction_type']]
             record.rate = Decimal(row[type_])
-            record.sourcing = sourcing
+            record.sourcing = 'intrastate' if 'intrastate' in type_ else 'interstate'
             record.rate_type = rate_type
             record.start_date = start_date
             record.end_date = None if end_date == dt.date.max else end_date
